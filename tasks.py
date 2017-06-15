1def0eb8ae62c8c77106d280d60ec4eb86dbd5e0
Nic Cheneweth <nchenewe@thoughtworks.com>
2017-06-15 16:34:50 -0500

    testing

--- tasks.py
+++ tasks.py
@@ -1,26 +1,21 @@
 from invoke import task
-from paramiko import SSHClient, AutoAddPolicy
-import os
+#from paramiko import SSHClient, AutoAddPolicy
+#import os
 
 @task
-def new(ctx):
-    ctx.run("terraform init -var-file=./bootstrap.tfvars", pty=True)
-    ctx.run("terraform env new bootstrap", pty=True)
+def init(ctx, environment):
+    ctx.run("terraform init -var-file=./{}.tfvars".format(environment), pty=True)
+    ctx.run("terraform env new {}".format(environment), pty=True)
 
-@task
-def init(ctx):
-    ctx.run("terraform init -var-file=./bootstrap.tfvars", pty=True)
-    ctx.run("terraform env select bootstrap", pty=True)
-
-@task
-def test(ctx):
-    ctx.run("bundle exec rspec spec", pty=True)
-
-@task
-def plan(ctx):
-    ctx.run("terraform get", pty=True)
-    ctx.run("terraform plan -var-file=./bootstrap.tfvars", pty=True)
-
-@task
-def apply(ctx):
-    ctx.run("terraform apply -var-file=./bootstrap.tfvars", pty=True)
+# @task
+# def test(ctx):
+#     ctx.run("bundle exec rspec spec", pty=True)
+#
+# @task
+# def plan(ctx):
+#     ctx.run("terraform get", pty=True)
+#     ctx.run("terraform plan -var-file=./bootstrap.tfvars", pty=True)
+#
+# @task
+# def apply(ctx):
+#     ctx.run("terraform apply -var-file=./bootstrap.tfvars", pty=True)
